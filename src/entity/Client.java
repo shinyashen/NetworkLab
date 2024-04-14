780037c407b80e45b9cdc69f298de551633e8216
shinya <shinyashen@qq.com>
2024-04-14 21:56:58 +0800

    Server and Client actions

--- src/entity/Client.java
+++ src/entity/Client.java
@@ -1,30 +1,104 @@
+//
+//import java.io.IOException;
+//import java.io.InputStream;
+//import java.io.OutputStream;
+//import java.net.Socket;
+//
+//public class Client {
+//    public static void main(String[] args) {
+//        InputStream inputStream;
+//        OutputStream outputStream;
+//        byte[] in_buf = new byte[1024];
+//        byte[] out_buf = new byte[1024];
+//        try {
+//            String server_ip = "200.5.3.1";
+//            Socket clientSocket = new Socket(server_ip, 8888);
+//            outputStream = clientSocket.getOutputStream();
+//            outputStream.write(out_buf);
+//            outputStream.flush();
+//            inputStream = clientSocket.getInputStream();
+//            int read_len = inputStream.read(in_buf);
+//            inputStream.close();
+//            outputStream.close();
+//            clientSocket.close();
+//
+//        } catch (IOException e) {
+//            throw new RuntimeException(e);
+//        }
+//    }
+//}
 package entity;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class Client {
+    private static Socket socket;
+    public static boolean socket_status=false;
     public static void main(String[] args) {
-        InputStream inputStream;
-        OutputStream outputStream;
-        byte[] in_buf = new byte[1024];
-        byte[] out_buf = new byte[1024];
-        try {
-            String server_ip = "200.5.3.1";
-            Socket clientSocket = new Socket(server_ip, 8888);
-            outputStream = clientSocket.getOutputStream();
-            outputStream.write(out_buf);
-            outputStream.flush();
-            inputStream = clientSocket.getInputStream();
-            int read_len = inputStream.read(in_buf);
-            inputStream.close();
-            outputStream.close();
-            clientSocket.close();
+        connect();
+    }
 
-        } catch (IOException e) {
-            throw new RuntimeException(e);
+    private static void connect() {
+        try {
+            socket = new Socket("127.0.0.1",8888);
+            socket_status = true;
+            ObjectOutputStream objectOutputStream=new ObjectOutputStream(socket.getOutputStream());
+            ObjectInputStream objectInputStream =new ObjectInputStream(socket.getInputStream());
+            new Thread(new Client_listen(socket,objectInputStream));
+            new Thread(new Client_send(socket,objectOutputStream));
+
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+}
+
+class Client_listen implements Runnable{
+    private Socket socket;
+    private ObjectInputStream objectInputStream;
+    Client_listen(Socket socket,ObjectInputStream objectInputStream){
+        this.socket=socket;
+        this.objectInputStream=objectInputStream;
+    }
+
+    @Override
+    public void run(){
+        try {
+            System.out.println(objectInputStream.readObject());
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+}
+
+class Client_send implements Runnable{
+
+    private Socket socket;
+    private ObjectOutputStream objectOutputStream;
+
+    Client_send(Socket socket,ObjectOutputStream objectOutputStream) {
+        this.socket=socket;
+        this.objectOutputStream=objectOutputStream;
+    }
+
+    @Override
+    public void run(){
+        try {
+            ObjectOutputStream objectOutputStream = new ObjectOutputStream(socket.getOutputStream());
+
+            Scanner scanner = new Scanner(System.in);
+            while (true) {
+                System.out.println("请客户端输入：");
+                String string = scanner.nextLine();
+
+                objectOutputStream.writeObject(string);
+                objectOutputStream.flush();
+            }
+        } catch (Exception e){
+            e.printStackTrace();
         }
     }
 }
--- src/entity/Server.java
+++ src/entity/Server.java
@@ -1,30 +1,104 @@
+//package entity;
+//
+//import java.io.IOException;
+//import java.io.InputStream;
+//import java.io.OutputStream;
+//import java.net.ServerSocket;
+//import java.net.Socket;
+//
+//public class Server {
+//    public static void main(String[] args) {
+//        InputStream inputStream;
+//        OutputStream outputStream;
+//        byte[] in_buf = new byte[1024];
+//        byte[] out_buf = new byte[1024];
+//        try {
+//            ServerSocket serverSocket = new ServerSocket(8888);
+//            Socket clientSocket = serverSocket.accept();
+//            inputStream = clientSocket.getInputStream();
+//            int read_len = inputStream.read(in_buf);
+//            outputStream = clientSocket.getOutputStream();
+//            outputStream.write(out_buf);
+//            outputStream.flush();
+//            inputStream.close();
+//            clientSocket.close();
+//            serverSocket.close();
+//        } catch (IOException e) {
+//            throw new RuntimeException(e);
+//        }
+//    }
+//
+//}
 package entity;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutput;
+import java.io.ObjectOutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class Server {
-    public static void main(String[] args) {
-        InputStream inputStream;
-        OutputStream outputStream;
-        byte[] in_buf = new byte[1024];
-        byte[] out_buf = new byte[1024];
+    public static void main(String[] args){
+        System.out.println("服务端已启动！");
         try {
-            ServerSocket serverSocket = new ServerSocket(8888);
-            Socket clientSocket = serverSocket.accept();
-            inputStream = clientSocket.getInputStream();
-            int read_len = inputStream.read(in_buf);
-            outputStream = clientSocket.getOutputStream();
-            outputStream.write(out_buf);
-            outputStream.flush();
-            inputStream.close();
-            clientSocket.close();
-            serverSocket.close();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
+            ServerSocket serverSocket=new ServerSocket(8888);
+            while (true) {
+                // 接受socket请求
+                Socket socket = serverSocket.accept();
+                new Thread(new Server_listen(socket)).start();
+                new Thread(new Server_send(socket)).start();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+class Server_listen implements Runnable {
+    private Socket socket;
+
+    Server_listen(Socket socket) {
+        this.socket = socket;
+    }
+
+    @Override
+    public void run(){
+        try {
+            ObjectInputStream objectInputStream = new ObjectInputStream(socket.getInputStream());
+
+            while (true) {
+                System.out.println(objectInputStream.readObject());
+            }
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+}
+
+class Server_send implements Runnable{
+
+    private Socket socket;
+
+    Server_send(Socket socket) {
+        this.socket=socket;
+    }
+
+    @Override
+    public void run(){
+        try {
+            ObjectOutputStream objectOutputStream = new ObjectOutputStream(socket.getOutputStream());
+
+            Scanner scanner = new Scanner(System.in);
+            while (true) {
+                System.out.println("请服务端输入：");
+                String string = scanner.nextLine();
+
+                objectOutputStream.writeObject(string);
+                objectOutputStream.flush();
+            }
+        } catch (Exception e){
+            e.printStackTrace();
         }
     }
 }
